0fb0b06d430b37ed3be56c8b964ff03f4cae1b40
Christopher Ray Langford <chris@hellocld.com>
2022-03-28 21:42:40 -0400

    Utilize csv, nice and easy export

--- ical_to_csv.py
+++ ical_to_csv.py
@@ -1,14 +1,29 @@
-from icalendar import Calendar, Event, vCalAddress, vText
-from datetime import datetime
-import os
-import pytz
+from ics import Calendar
+import csv
 import requests
 
-
 url = "https://www.meetup.com/BuffaloGameSpace/events/ical/"
-
 r = requests.get(url)
+#cal = Calendar.from_ical(r.content)
+cal = Calendar(r.text)
 
-cal = Calendar.from_ical(r.content)
+with open('cal_parsed.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for event in cal.events:
+        if event.status == "CONFIRMED":
+            writer.writerow(
+                [
+                    "kind", 
+                    event.name, 
+                    event.begin.date(), 
+                    "1", 
+                    "1", 
+                    event.begin.time().strftime("%-I:%M%p"),
+                    event.end.time().strftime("%-I:%M%p"), 
+                    event.location
+                ]
+            )
 
-print(cal)
+
+
+
